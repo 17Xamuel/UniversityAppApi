15f6f415f0ecefb9b7a193581e8bcf6760f24273
XamuelUG <samweko@gmail.com>
2021-10-10 02:05:46 +0300

    added login and register

--- api/student.js
+++ api/student.js
@@ -4,17 +4,60 @@ const conn = require("../database/mysql");
 router.post("/new", (req, res) => {
   let { username, password } = req.body;
   conn.query(
-    "INSERT INTO students_table SET ?",
-    {
-      student_username: username,
-      student_password: password,
-    },
+    "SELECT * FROM students_table WHERE student_username = ?",
+    username,
+    (error, res) => {
+      if (error) {
+        res.send({ data: "Error" });
+        throw error;
+      } else {
+        if (res.length > 0) {
+          res.send({ data: "Exits" });
+        } else {
+          conn.query(
+            "INSERT INTO students_table SET ?",
+            {
+              student_username: username,
+              student_password: password,
+            },
+            (err, result) => {
+              if (err) {
+                res.send({ data: "Error" });
+                throw err;
+              } else {
+                res.send({
+                  data: "true",
+                  student_username: username,
+                  student_password: password,
+                });
+              }
+            }
+          );
+        }
+      }
+    }
+  );
+});
+
+router.post("/login", (req, res) => {
+  let { username, password } = req.body;
+  conn.query(
+    "SELECT * FROM students_table WHERE student_username = ? AND student_password = ?",
+    [username, password],
     (err, result) => {
       if (err) {
-        res.send("Not Inserted");
+        res.send({ data: "Error" });
         throw err;
       } else {
-        res.send("Inserted");
+        if (result.length > 0) {
+          res.send({
+            data: "true",
+            student_username: username,
+            student_password: password,
+          });
+        } else {
+          res.send({ data: "Wrong_details" });
+        }
       }
     }
   );
