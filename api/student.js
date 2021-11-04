44de82894b93eacf61319adbd7417db7aaa81189
MulindwaDenis13 <mulindwadenis13@gmail.com>
2021-11-04 07:12:11 +0300

    payment

--- api/student.js
+++ api/student.js
@@ -42,7 +42,8 @@ router.post("/new", (req, res) => {
 router.post("/login", (req, res) => {
   let { username, password } = req.body;
   conn.query(
-    "SELECT * FROM students_table WHERE student_username = ? AND student_password = ?",
+    `SELECT * FROM students_table WHERE 
+    student_username = ? AND student_password = ?`,
     [username, password],
     (err, result) => {
       if (err) {
@@ -58,6 +59,55 @@ router.post("/login", (req, res) => {
         } else {
           res.send({ data: "Wrong_details" });
         }
+      }
+    }
+  );
+});
+
+router.post("/new-payment", async (req, res) => {
+  const { amount, student_id, photo, date } = req.body;
+  conn.query(
+    `INSERT INTO payments_table SET ?`,
+    {
+      student_id: student_id,
+      amount_paid: amount,
+      student_photo: photo,
+      payment_date: date,
+    },
+    (payment_err, payment_res) => {
+      if (payment_err) {
+        console.log(payment_err);
+        res.send({ data: "Error" });
+      } else {
+        conn.query(
+          `SELECT * FROM payments_table 
+          WHERE student_id = ?`,
+          student_id,
+          (first_err, first_res) => {
+            if (first_err) {
+              console.log(first_err);
+              res.send({ data: "Error" });
+            } else {
+              res.send({ data: first_res });
+            }
+          }
+        );
+      }
+    }
+  );
+});
+
+router.get("/payments/:id", async (req, res) => {
+  conn.query(
+    `SELECT * FROM payments_tbl 
+    WHERE student_id = ?`,
+    req.params.id,
+    (first_err, first_res) => {
+      if (first_err) {
+        console.log(first_err);
+        res.send({ data: "Error" });
+      } else {
+        res.send({ data: first_res });
       }
     }
   );
